c439b17f9b18a78f742fc8edb247d2118dc7c1cd
Tifanny-S <serafin.tifanny@mail.com>
2025-05-01 12:19:54 +0200

    création bouton Ajouter une tache + création bouton filtrant - A faire : créer les filtres + ajouter le lien vers la page dédié pour le bouton ajouter

---
+++ src/components/TaskList.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-row justify="center">
+        <v-col cols="auto">
+             <v-btn color="primary" class="addTaskBtn">Ajouter une nouvelle tâche</v-btn>
+        </v-col>
+    </v-row>
+    <v-row justify="center">
+        <v-col cols="auto">
+            <v-btn color="#E5E7EB" size="small">Toutes</v-btn>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn color="#E5E7EB" size="small">En attentes</v-btn>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn color="#E5E7EB" size="small">En cours</v-btn>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn color="#E5E7EB" size="small">Terminés</v-btn>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn color="#E5E7EB" size="small">Bloqués</v-btn>
+        </v-col>
+    </v-row>
+    <TasksCard />
+</template>
+
+<script>
+import TasksCard from '@/components/TasksCard.vue'
+
+export default {
+  name: 'TaskList',
+  components: {
+    TasksCard
+  }
+}
+</script>
+
+<style>
+.addTaskBtn {
+    margin-left: 10%;
+    margin-bottom: 30px;
+}
+
+</style>
