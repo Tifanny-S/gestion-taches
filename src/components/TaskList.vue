9cc475409ca9bd2f5d6e8d75f69d16108f4af99b
Tifanny-S <serafin.tifanny@mail.com>
2025-05-01 12:57:12 +0200

    Ajout du chemin vers la page Ajouter une tache + remise en forme du code

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,27 +1,29 @@
 <template>
-    <v-row justify="center">
-        <v-col cols="auto">
-             <v-btn color="primary" class="addTaskBtn">Ajouter une nouvelle tâche</v-btn>
-        </v-col>
-    </v-row>
-    <v-row justify="center">
-        <v-col cols="auto">
-            <v-btn color="#E5E7EB" size="small">Toutes</v-btn>
-        </v-col>
-        <v-col cols="auto">
-            <v-btn color="#E5E7EB" size="small">En attentes</v-btn>
-        </v-col>
-        <v-col cols="auto">
-            <v-btn color="#E5E7EB" size="small">En cours</v-btn>
-        </v-col>
-        <v-col cols="auto">
-            <v-btn color="#E5E7EB" size="small">Terminés</v-btn>
-        </v-col>
-        <v-col cols="auto">
-            <v-btn color="#E5E7EB" size="small">Bloqués</v-btn>
-        </v-col>
-    </v-row>
-    <TasksCard />
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="auto">
+                <v-btn color="primary" class="addTaskBtn" link :to="{path: '/tache'}">Ajouter une nouvelle tâche</v-btn>
+            </v-col>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="auto">
+                <v-btn color="#E5E7EB" size="small">Toutes</v-btn>
+            </v-col>
+            <v-col cols="auto">
+                <v-btn color="#E5E7EB" size="small">En attentes</v-btn>
+            </v-col>
+            <v-col cols="auto">
+                <v-btn color="#E5E7EB" size="small">En cours</v-btn>
+            </v-col>
+            <v-col cols="auto">
+                <v-btn color="#E5E7EB" size="small">Terminés</v-btn>
+            </v-col>
+            <v-col cols="auto">
+                <v-btn color="#E5E7EB" size="small">Bloqués</v-btn>
+            </v-col>
+        </v-row>
+        <TasksCard />
+    </v-container>
 </template>
 
 <script>
