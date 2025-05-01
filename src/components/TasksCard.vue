ba5abe3e9b33c66c5391ce3bd0f56fbc0fb35334
Tifanny-S <serafin.tifanny@mail.com>
2025-05-01 11:51:34 +0200

    creation du composant + ajout d'une card statique

---
+++ src/components/TasksCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="cards">
+        <v-card>
+            <v-card-title>Nom de la tâche</v-card-title>
+            <v-card-subtitle>Etat</v-card-subtitle>
+            <v-card-text>Détails</v-card-text>
+            <v-card-text>Crée le 01/05/2025</v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TasksCard',
+}
+
+</script>
+
+<style>
+.cards {
+    margin-top: 5%;
+    margin-left: 10%;
+    width: 50%;
+    border-left: 5px solid #3B82F6;
+}
+</style>
